46a7528af4cbb501e989853fa608d2cc4e3c26b6
loken9387 <loken937@gmail.com>
2023-10-26 10:49:25 -0400

    First draft

--- webserver.py
+++ webserver.py
@@ -5,43 +5,39 @@ import sys # In order to terminate the program
 serverSocket = socket(AF_INET, SOCK_STREAM)
 
 #Prepare a sever socket
-#Fill in start
-#Fill in end
+serverPort = 6789
+serverSocket.bind(('', serverPort))
+serverSocket.listen(1)
 
 while True:
 	#Establish the connection
 	print('Ready to serve...')
-	connectionSocket, addr = #Fill in start #Fill in end
+	connectionSocket, addr = serverSocket.accept()
 	
 	try:
-		
-		message = #Fill in start #Fill in end
+		# Receive the HTTP request	
+		message = connectionSocket.recv(1024).decode()
 		
 		filename = message.split()[1]
 		f = open(filename[1:])
-		outputdata = #Fill in start #Fill in end
+		outputdata = f.read()
 		
 		#Send one HTTP header line into socket
-		
-		#Fill in start
-		#Fill in end
+		connectionSocket.send("HTTP/1.1 200 OK\r\n\r\n".encode())
 		
 		#Send the content of the requested file to the client
 		for i in range(0, len(outputdata)):
 			connectionSocket.send(outputdata[i].encode())
-			connectionSocket.send("\r\n".encode())
-			connectionSocket.close()
-	
+		connectionSocket.send("\r\n".encode())
+			
 	except IOError:
-		#Send response message for file not found
-		print("poop")
-		#Fill in start
-		#Fill in end
-
-		#Close client socket
-
-		#Fill in start
-		#Fill in end
+		# Send response message for file not found
+		connectionSocket.send("HTTP/1.1 404 Not Found\r\n\r\n".encode())
+		connectionSocket.send("<html><head></head><body><h1>404 Not Found</h1></body></html>\r\n".encode())
+    
+	finally:
+        # Close client socket
+		connectionSocket.close()
 
 serverSocket.close()
 sys.exit()#Terminate the program after sending the corresponding data
